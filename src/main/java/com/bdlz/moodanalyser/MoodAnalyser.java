1b10c8d1a4b8b2a657df79429209b6845f3c387b
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-18 22:35:40 +0530

    Added: Sad Mood

--- src/main/java/com/bdlz/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/bdlz/moodanalyser/MoodAnalyser.java
@@ -1,19 +1,11 @@
 package com.bdlz.moodanalyser;
 
 public class MoodAnalyser {
+
     public static String analyseMood(String message) {
-        if (message.toLowerCase().contains("happy")) {
-            return "Happy";
-        } else if (message.toLowerCase().contains("sad")) {
-            return "Sad";
+        if (message.toLowerCase().contains("sad")) {
+            return "SAD";
         } else
             return null;
-    }
-
-    public static void main(String[] args) {
-        String mood = MoodAnalyser.analyseMood("I am Happy");
-        System.out.println(mood);
-        mood = MoodAnalyser.analyseMood("I am Sad");
-        System.out.println(mood);
     }
 }
---
+++ src/test/java/com/bdlz/moodanalyser/MoodAnalyserTest.java
@@ -0,0 +1,15 @@
+package com.bdlz.moodanalyser;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MoodAnalyserTest {
+    @Test
+    public void givenMessage_IsProper_ShouldReturnSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String actualResult = moodAnalyser.analyseMood("I am Sad in Mood");
+        Assert.assertEquals("SAD", actualResult);
+    }
+}
