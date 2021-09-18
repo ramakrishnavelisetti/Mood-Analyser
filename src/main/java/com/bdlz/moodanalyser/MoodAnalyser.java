720d0e77e0bfbd7207087ec0fc5449e72082dea6
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-18 21:58:27 +0530

    Added: Welcome Message

---
+++ src/main/java/com/bdlz/moodanalyser/MoodAnalyser.java
@@ -0,0 +1,7 @@
+package com.bdlz.moodanalyser;
+
+public class MoodAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Mood Analyser Program");
+    }
+}
