0add45f370940dd4284e88dc03d5c36d8762673e
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-18 22:17:18 +0530

    Added: Happy Or Sad

--- src/main/java/com/bdlz/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/bdlz/moodanalyser/MoodAnalyser.java
@@ -1,7 +1,19 @@
 package com.bdlz.moodanalyser;
 
 public class MoodAnalyser {
+    public static String analyseMood(String message) {
+        if (message.toLowerCase().contains("happy")) {
+            return "Happy";
+        } else if (message.toLowerCase().contains("sad")) {
+            return "Sad";
+        } else
+            return null;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the Mood Analyser Program");
+        String mood = MoodAnalyser.analyseMood("I am Happy");
+        System.out.println(mood);
+        mood = MoodAnalyser.analyseMood("I am Sad");
+        System.out.println(mood);
     }
 }
