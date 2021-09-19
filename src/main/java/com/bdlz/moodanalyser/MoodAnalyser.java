878cff647b0bd51e9213f609201c195cd922997e
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-19 12:21:15 +0530

    Added: Refactor code to constructor

--- src/main/java/com/bdlz/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/bdlz/moodanalyser/MoodAnalyser.java
@@ -1,8 +1,25 @@
 package com.bdlz.moodanalyser;
 
 public class MoodAnalyser {
+    public static String message;
 
-    public static String analyseMood(String message) {
+    public MoodAnalyser(String message)  {
+        this.message = message;
+    }
+
+    public static String getMessage() {
+        return message;
+    }
+
+    public static void setMessage(String message) {
+
+        MoodAnalyser.message = message;
+    }
+    public MoodAnalyser() {
+
+    }
+
+    public static String analyseMood() {
         if (message.toLowerCase().contains("sad")) {
             return "SAD";
         } else if (message.toLowerCase().contains("any")) {
--- src/test/java/com/bdlz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bdlz/moodanalyser/MoodAnalyserTest.java
@@ -2,20 +2,19 @@ package com.bdlz.moodanalyser;
 
 import org.junit.Assert;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class MoodAnalyserTest {
     @Test
     public void givenMessage_IsProper_ShouldReturnSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String actualResult = moodAnalyser.analyseMood("I am Sad in Mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
+        String actualResult = moodAnalyser.analyseMood();
         Assert.assertEquals("SAD", actualResult);
     }
     @Test
     public void givenMessage_IsProper_AndContain_Any_ShouldReturnHappy() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String actualResult = moodAnalyser.analyseMood("I am in Any Mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Any Mood");
+        String actualResult = moodAnalyser.analyseMood();
         Assert.assertEquals("HAPPY", actualResult);
     }
 }
