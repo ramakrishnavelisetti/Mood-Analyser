6ffffdf6d777f5dfd8a63617c8568631abb52ba8
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-19 12:30:33 +0530

    Added: By Constructor Return Sad

--- src/test/java/com/bdlz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bdlz/moodanalyser/MoodAnalyserTest.java
@@ -7,14 +7,10 @@ import static org.junit.Assert.*;
 public class MoodAnalyserTest {
     @Test
     public void givenMessage_IsProper_ShouldReturnSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        MoodAnalyser.setMessage("I am in Sad Mood");
         String actualResult = moodAnalyser.analyseMood();
         Assert.assertEquals("SAD", actualResult);
     }
-    @Test
-    public void givenMessage_IsProper_AndContain_Any_ShouldReturnHappy() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Any Mood");
-        String actualResult = moodAnalyser.analyseMood();
-        Assert.assertEquals("HAPPY", actualResult);
-    }
+
 }
